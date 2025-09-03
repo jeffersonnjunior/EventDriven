2f8642a922e94a24c5aceb4aaf3a4e3992becb54
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-09-03 15:04:30 -0300

    feat: add docker-componse

---
+++ MembershipPlanManagement/Infrastructure/DependencyInjection/AddPersistenceDependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Persistence.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DependencyInjection;
+
+public static class AddPersistenceDependencyInjection
+{
+    public static IServiceCollection PersistenceDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<MongoContext>(provider => new MongoContext(configuration));
+
+        return services;
+    }
+}
---
+++ MembershipPlanManagement/Infrastructure/Persistence/Context/MongoContext.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Persistence.Context;
+
+public class MongoContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoContext(IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("MongoDb");
+        var databaseName = configuration["MongoDbSettings:Database"];
+        var client = new MongoClient(connectionString);
+        _database = client.GetDatabase(databaseName);
+    }
+
+    public IMongoCollection<T> GetCollection<T>(string name)
+    {
+        return _database.GetCollection<T>(name);
+    }
+}
