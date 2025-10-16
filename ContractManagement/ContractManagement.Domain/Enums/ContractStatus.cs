7cb5e399ab3196604a4bedb1e9fdcf151fb042c4
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-10-16 10:06:17 -0300

    feat: add enum

---
+++ ContractManagement/ContractManagement.Domain/Enums/ContractStatus.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace ContractManagement.Domain.Enums;
+
+public enum ContractStatus
+{
+    [BsonElement("draft")]
+    Draft,
+    
+    [BsonElement("active")]
+    Active,
+    
+    [BsonElement("suspended")]
+    Suspended,
+    
+    [BsonElement("terminated")]
+    Terminated,
+    
+    [BsonElement("expired")]
+    Expired,
+    
+    [BsonElement("under_review")]
+    UnderReview
+}
