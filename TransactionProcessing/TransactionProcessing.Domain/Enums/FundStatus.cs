83a58aabfebbe6c69ca349a7a6fb762d76ad06bf
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-10-16 10:09:43 -0300

    feat: add enum

---
+++ TransactionProcessing/TransactionProcessing.Domain/Enums/FundStatus.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace TransactionProcessing.Domain.Enums;
+
+public enum FundStatus
+{
+    [BsonElement("active")]
+    Active,
+
+    [BsonElement("inactive")]
+    Inactive,
+
+    [BsonElement("liquidating")]
+    Liquidating,
+
+    [BsonElement("closed")]
+    Closed,
+
+    [BsonElement("suspended")]
+    Suspended,
+
+    [BsonElement("under_review")]
+    UnderReview
+}
