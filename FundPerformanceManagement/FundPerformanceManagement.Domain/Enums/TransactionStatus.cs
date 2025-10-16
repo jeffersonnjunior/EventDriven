d07790771707ea4c75524da515d83feb0dee6f07
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-10-16 10:08:44 -0300

    feat: add enum

---
+++ FundPerformanceManagement/FundPerformanceManagement.Domain/Enums/TransactionStatus.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace FundPerformanceManagement.Domain.Enums;
+
+public enum TransactionStatus
+{
+    [BsonElement("pending")]
+    Pending,
+
+    [BsonElement("processing")]
+    Processing,
+
+    [BsonElement("completed")]
+    Completed,
+
+    [BsonElement("failed")]
+    Failed,
+
+    [BsonElement("cancelled")]
+    Cancelled,
+
+    [BsonElement("on_hold")]
+    OnHold
+}
