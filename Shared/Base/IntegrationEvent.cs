d02117c8ee19cdba51977cde5d5db45e8db3a173
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-10-22 09:29:49 -0300

    feat: add shared

---
+++ Shared/Base/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Base;
+
+public abstract class IntegrationEvent
+{
+    public Guid EventId { get; } = Guid.NewGuid();
+    public DateTime Timestamp { get; } = DateTime.UtcNow;
+    public int Version { get; } = 1;
+}
