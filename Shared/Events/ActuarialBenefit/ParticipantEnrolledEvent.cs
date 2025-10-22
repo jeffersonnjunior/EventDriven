b738ec270bf8a6db071e57dee56444bb5a79f711
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-10-22 09:43:09 -0300

    feat: add shared

---
+++ Shared/Events/ActuarialBenefit/ParticipantEnrolledEvent.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Base;
+
+namespace Shared.Events.ActuarialBenefit;
+
+public class ParticipantEnrolledEvent : IntegrationEvent
+{
+    public string ParticipantId { get; init; }
+    public string RegistrationNumber { get; init; }
+    public string PlanId { get; init; }    
+    public string SponsorId { get; init; } 
+    public decimal BaseSalary { get; init; }
+    public DateTime EnrollmentDate { get; init; }
+
+    public ParticipantEnrolledEvent(string participantId, string regNumber, string planId, string sponsorId, decimal baseSalary, DateTime enrollmentDate)
+    {
+        ParticipantId = participantId;
+        RegistrationNumber = regNumber;
+        PlanId = planId;
+        SponsorId = sponsorId;
+        BaseSalary = baseSalary;
+        EnrollmentDate = enrollmentDate;
+    }
+}
