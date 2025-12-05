f3fc8cbfecbfe5fdd2c93e0de042dca41cc3f7db
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-12-05 09:53:00 -0300

    feat: add exceptions

---
+++ PlanManagement/PlanManagement.Domain/Exceptions/NotificationContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Exceptions;
+
+public class NotificationContext
+{
+    private readonly List<NotificationModel> _notifications;
+
+    public NotificationContext()
+    {
+        _notifications = new List<NotificationModel>();
+    }
+    public IReadOnlyCollection<NotificationModel> GetNotifications() => _notifications.AsReadOnly();
+    public void AddNotification(string key, string message) => _notifications.Add(new NotificationModel(key, message));
+    public bool HasNotifications() => _notifications.Count > 0;
+}
---
+++ PlanManagement/PlanManagement.Domain/Exceptions/NotificationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Exceptions;
+
+public class NotificationModel
+{
+    public string Key { get; set; }
+    public string Message { get; }
+
+    public NotificationModel(string key, string message)
+    {
+        Key = key;
+        Message = message;
+    }
+}
