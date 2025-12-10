17f29b018af4c33cb37076ce2e8fb8bdacf775f7
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-12-10 08:51:30 -0300

    feat: add swagger

--- PlanManagement/PlanManagement.Api/Program.cs
+++ PlanManagement/PlanManagement.Api/Program.cs
@@ -1,17 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "PlanManagement API v1");
+        options.RoutePrefix = "swagger"; 
+    });
 }
 
 app.UseHttpsRedirection();
